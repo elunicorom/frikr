3979c0c4b8abc4506808e97d3b1ffe16992696d6
Ton <barsa_real_31@hotmail.com>
2016-11-25 00:31:59 -0500

    Configuracion de BD hecha y creada

--- frikr/settings.py
+++ frikr/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'frikr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testeo',
+        'USER': 'oliver',
+        'PASSWORD' : 'yugiho2000',
+        'HOST' : 'localhost',
+        'PORT' : 5432,
     }
 }
 
