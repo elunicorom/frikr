d6841fe547c93b4ca6a276c1367903b70022d66f
Ton <barsa_real_31@hotmail.com>
2016-11-25 20:50:23 -0500

    URl agregado

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+# vistas son controladores :v --- Manejan Url
+def home(request):
+    return HttpResponse("Hola a todos :v")
