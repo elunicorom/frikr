f73b79e8cc0348c20d8c33278372286c0b96e4dd
Oliver <barsa_real_31@hotmail.com>
2016-12-14 17:57:33 -0500

    agregando

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from photos.models import Photo
+from photos.models import Photo,PUBLIC
 
 
 # vistas son controladores :v --- Manejan Url
 def home(request):
-	photos = Photo.objects.all().order_by('-created_at')
+	photos = Photo.objects.filter(visibility=PUBLIC).order_by('-created_at')
 	context = {
 		'photos_list': photos
 	}
 	return render(request,'photos/home.html',context)
+
+def detail(request,pk):
+	"""
+	carga la pagina de detalle de una foto
+	:param request: Httpresponse
+	:param pk: id de la foto
+	:return  Httpresponse
+	"""
+	"""
+	try:
+		photo = Photo.objects.get(pk=pk)
+	except Photo.DoesNotExist:
+		photo=None
+	except Photo.MultipleObjects:
+		photo=None
+	"""
+	possible_photos = Photos.objects.filter(pk=pk)
+	photo
