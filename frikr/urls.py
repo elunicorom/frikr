442abab3417fbd459e0cf99ba0ada565ede52e20
Oliver <barsa_real_31@hotmail.com>
2016-12-13 20:06:13 -0500

    objetos traidos de la BD

--- frikr/urls.py
+++ frikr/urls.py
@@ -20,7 +20,6 @@ from photos import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^$',views.home,name='home'),
 
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from photos.models import Photo
 
 
 # vistas son controladores :v --- Manejan Url
 def home(request):
-    return HttpResponse("Hola a todos :v")
+	photos = Photo.objects.all()
+	html = '<ul>'
+	for photo in photos:
+		html+='<li>' + photo.name +'</li>'
+	html+= '</ul>'
+	return HttpResponse(html)
