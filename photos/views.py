fcb845fcda3cdceab3e3e439928faefa2486b68a
Oliver <barsa_real_31@hotmail.com>
2016-12-14 16:21:35 -0500

    Imagenes correspondientes a cada foto agregadas

--- photos/views.py
+++ photos/views.py
@@ -6,8 +6,7 @@ from photos.models import Photo
 # vistas son controladores :v --- Manejan Url
 def home(request):
 	photos = Photo.objects.all()
-	html = '<ul>'
-	for photo in photos:
-		html+='<li>' + photo.name +'</li>'
-	html+= '</ul>'
-	return HttpResponse(html)
+	context = {
+		'photos_list': photos
+	}
+	return render(request,'photos/home.html',context)
