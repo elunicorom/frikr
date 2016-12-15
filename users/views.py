1ae587c21f0b16b33619920eda0f5ba23493cf1f
Oliver <barsa_real_31@hotmail.com>
2016-12-15 01:03:25 -0500

    implementacion del login completada junto con sus medidas de seguridad

--- users/views.py
+++ users/views.py
@@ -1,8 +1,26 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render,redirect
-from django.contrib.auth import logout as django_logout
+from django.contrib.auth import logout as django_logout,authenticate,login as django_login
 # Create your views here.
 def login(request):
-	return render(request,'users/login.html')
+	error_messages=[]
+	if request.method=='POST':
+		username=request.POST.get('usr')
+		password=request.POST.get('pwd')
+		user=authenticate(username=username,password=password)
+		if user is None:
+			error_messages.append('Nombre de usuario o contraseña incorrecta')
+		else:
+			if user.is_active:
+				django_login(request,user)
+				return redirect('photos_home')	
+			else:
+				error_messages.append('El usuario no esta activo')
+	context={
+		'errors': error_messages
+	}
+
+	return render(request,'users/login.html',context)
 
 def logout(request):
 	if request.user.is_authenticated():
