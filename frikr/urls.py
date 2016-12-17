66ab0d1976dc2f1bad0f8ec7036cc64a38472840
Oliver <barsa_real_31@hotmail.com>
2016-12-16 22:49:32 -0500

    bootstrap implementado

--- frikr/urls.py
+++ frikr/urls.py
@@ -18,12 +18,13 @@ from django.conf.urls import include,url
 from django.contrib import admin
 from photos.views import DetailView,CreateView,HomeView,PhotoListView,UserPhotosView
 from users.views import LoginView,LogoutView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 #Photos URL
     url(r'^$',HomeView.as_view(),name='photos_home'),
-    url(r'^my-photos/$',UserPhotosView.as_view(),name='user_photos'),
+    url(r'^my-photos/$',login_required(UserPhotosView.as_view()),name='user_photos'),
     url(r'^photos/$',PhotoListView.as_view(),name='photos_list'),
     url(r'^photos/(?P<pk>[0-9]+)$',DetailView.as_view(),name='photos_detail'),
     url(r'^photos/new$',CreateView.as_view(),name='create_photo'), 
