d8b1650a5287bb8f20255bda955835965d495886
Oliver <barsa_real_31@hotmail.com>
2016-12-17 14:38:45 -0500

    APIView colocado

--- users/api.py
+++ users/api.py
@@ -4,14 +4,13 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from users.serializers import UserSerializer
 from rest_framework.renderers import JSONRenderer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-class UserListAPI(View):
+class UserListAPI(APIView):
 
 	def get(self,request):
 		users=User.objects.all()
 		serializer=UserSerializer(users,many=True)
 		serializer_users=serializer.data #lista de diccionarios
-		renderer = JSONRenderer()
-		json_users=renderer.render(serializer_users)#lista de diccionarios ->JSON
-		return HttpResponse(json_users)
-
+		return Response(serializer_users)
