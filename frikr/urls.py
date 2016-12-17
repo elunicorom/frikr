3a6a6c0c439c2c18a280a97dab4a5804a4475be7
Oliver <barsa_real_31@hotmail.com>
2016-12-17 14:50:56 -0500

    UserDetailAPI implementada

--- frikr/urls.py
+++ frikr/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include,url
 from django.contrib import admin
 from photos.views import DetailView,CreateView,HomeView,PhotoListView,UserPhotosView
 from users.views import LoginView,LogoutView
-from users.api import UserListAPI
+from users.api import UserListAPI,UserDetailAPI
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
@@ -36,7 +36,8 @@ urlpatterns = [
     url(r'^logout$',LogoutView.as_view(),name='users_logout'),
 
 #Users API URL
-    url(r'^api/1.0/users/$',UserListAPI.as_view(),name='user_list_api')
+    url(r'^api/1.0/users/$',UserListAPI.as_view(),name='user_list_api'),
+    url(r'^api/1.0/users/(?P<pk>[0-9]+)$',UserDetailAPI.as_view(),name='user_detail_api')
 
 
 ]
--- users/api.py
+++ users/api.py
@@ -6,6 +6,7 @@ from users.serializers import UserSerializer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class UserListAPI(APIView):
 
@@ -14,3 +15,11 @@ class UserListAPI(APIView):
 		serializer=UserSerializer(users,many=True)
 		serializer_users=serializer.data #lista de diccionarios
 		return Response(serializer_users)
+
+
+class UserDetailAPI(APIView):
+
+	def get(self,request,pk):
+		user = get_object_or_404(User,pk=pk)
+		serializer=UserSerializer(user)
+		return Response(serializer.data)
